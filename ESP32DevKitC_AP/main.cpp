3da8a7fa0f025d8b93df8644da2ccebdc239de52
ElectricalThinking29 <140239795+ElectricalThinking29@users.noreply.github.com>
2023-12-31 18:39:18 +0700

    Update main.cpp

--- ESP32DevKitC_AP/main.cpp
+++ ESP32DevKitC_AP/main.cpp
@@ -1,4 +1,36 @@
-#include <Arduino.h>
+/*** MY CHANNEL ***
+If you can, please support me on my Youtube's channel:
+
+    www.youtube.com/@TheElectricTHINK
+
+by hitting like, share and subscribe's buttons to my videos.
+
+*/
+
+/*** LICENSE ***
+
+Copyright 2023 @ElectricalThinking29
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+*/
+
+/*** DISCRIPTION ***
+  This is the program for ESP32-DevKitC board of my "second" 
+  Measure-distance-using-RSSI project.
+*/
+
+#include <Arduino.h> // If you use Arduino IDE or something else, delete this line
 #include <WiFi.h>
 
 #define LED_BUILTIN 2
